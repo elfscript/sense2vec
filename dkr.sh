3800a9c321e9c2f9038c91abfcd3ed4a81f1a921
3hdeng <3hdeng@gmail.com>
2017-04-23 14:38:53 +0000

    reddit-vectors data placed on sense2vec/data; spacy  data placed on ~/workspaces/spacy/data

--- dkr.sh
+++ dkr.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 docker run -it --rm  --name mysense2vec \
   -v $(pwd):/mnt/work  -w /mnt/work \
+  -v $HOME/workpsaces/spacy/data:/usr/local/lib/python2.7/dist-packages/spacy/data \
   3hdeng/sense2vec:2.7 \
   /bin/bash
 
